3a0601cdbffa5e87f69c9367f7e1debf26d697a9
Esther-mac <esther_lin@mail.chungyo.net>
2020-08-25 11:28:12 +0800

    update index(toggle class)

--- bank_system/index.php
+++ bank_system/index.php
@@ -1,5 +1,13 @@
 <?php 
-    
+    require ("config.php");
+
+    $sql = "select firstName, lastName, accountBalance  
+    FROM `customer` c join `account` a
+    ON c.customerId = a.customerId";
+
+    $result = mysqli_query($link,$sql);
+    $row = mysqli_fetch_assoc($result);
+
 ?>
 
 <!DOCTYPE html>
@@ -37,13 +45,17 @@
     <li class="nav-item">
         <a class="nav-link" href="#">查詢明細</a>
     </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">登出</a>
+    </li>
     </ul>
+
     <div id="main" class="row">
         <div class="col"></div>
         <div id="content" class="col-8">
-            <h2 id="welcome">Hi,XXX</h2>
-            <h3>帳戶餘額: <label id="money">1000</label>元
-            <a href="#" role="button"><span class="fa fa-eye-slash"></span></span></a></h3>
+            <h2 id="welcome">Hi,<span><?= $row['firstName'] ?></sapn></h2>
+            <h3>帳戶餘額: <label id="money"><?= $row['accountBalance']?></label>元
+            <a id="iconEye" href="#" role="button"><span class="fa fa-eye-slash"></span></span></a></h3>
 
             <div id="actionBtn">
                 <a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">提款</a>
@@ -55,6 +67,16 @@
 </div>
 </body>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    
+    <script>
+        $(function(){
+            $("#iconEye").click(function(){
+                $("#iconEye span").toggleClass("fa fa-eye");
+                $("#money").text("＊＊＊");
+            })
+            //這裡還要再研究
+        })
+    </script>
     
 </html>
